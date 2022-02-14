9fa8cd6cf65dcebb36bcf4d0090133a2bbd1ed33
tguthrie1765 <84353446+tguthrie1765@users.noreply.github.com>
2022-02-14 12:38:12 -0800

    feb-14

--- src/Search.js
+++ src/Search.js
@@ -4,17 +4,22 @@ import { bookData } from "./Data";
 import { searchByKeyword } from "./utl";
 
 // main component
-// has contains assgined state for button selection
-//contains input state
+// calls child componets
 export default function Search() {
-  const [selection, setSelection] = useState("Keyword");
-  const [input, setInput] = useState("");
+  //const [selection, setSelection] = useState("Keyword");
+  //const [input, setInput] = useState("");
+
+  const [valueSearch, setValueSerach] = useState("");
+  const [valueDrop, setValueDrop] = useState("");
+  console.log("PARENT VALUE DROPDOWN=", { valueDrop });
+  console.log("PARENT VALUE SEARCH INPUT=", { valueSearch });
 
   return (
     <div className="Home">
       <br />
       <h1>LibrarySearch </h1>
       <br />
+      {/*
       <div className="dropdown">
         <button className="dropbutton">{selection}</button>
         <div className="dropdown-content">
@@ -23,6 +28,7 @@ export default function Search() {
           <button onClick={() => setSelection("author")}>Author</button>
         </div>
       </div>
+      
       <input
         type="text"
         id="test"
@@ -30,19 +36,76 @@ export default function Search() {
         onChange={(e) => setInput(e.target.value)}
         value={input}
       />
+      */}
       {/* TODO: Make so when clicked it calls search function*/}
+      {/*
       <button onClick={() => {}} className="mGlass">
         <span role="img" aria-label="search glass">
           {" "}
           &#x1F50E;
         </span>
       </button>
+      */}
       {/* calls the search function. 
-    TODO: make so when it prints it looks better*/}
+    TODO: make so when it prints it looks better
       {JSON.stringify(searchByKeyword(bookData, selection, input))}
+*/}
 
+      <Inputs setValueSerach={setValueSerach} setValueDrop={setValueDrop} />
+      <SearchResults valueSearch={valueSearch} valueDrop={valueDrop} />
       <div className="UO">
         <img src="UO.png" alt="UO" />
+      </div>
+    </div>
+  );
+}
+
+// holds the input buttons and forms
+//sets state
+//lifts state to parent
+//clears local state
+function Inputs(props) {
+  const { setValueSerach } = props;
+  const { setValueDrop } = props;
+  const [selection, setSelection] = useState("Keyword");
+  const [input, setInput] = useState("");
+
+  function submit() {
+    //if empty do help statement
+    //else lift the state
+    setValueSerach(input);
+    setValueDrop(selection);
+  }
+
+  return (
+    <div className="dropdown">
+      <button className="dropbutton">{selection}</button>
+      <div className="dropdown-content">
+        <button onClick={() => setSelection("keyword")}>Keyword</button>
+        <button onClick={() => setSelection("title")}>Title</button>
+        <button onClick={() => setSelection("author")}>Author</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          id="test"
+          placeholder="Search..."
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            submit();
+          }}
+          className="mGlass"
+        >
+          <span role="img" aria-label="search glass">
+            {" "}
+            &#x1F50E;
+          </span>
+        </button>
       </div>
     </div>
   );
@@ -52,6 +115,7 @@ export default function Search() {
 //use filtering to remove any objects from
 // data array that do not match input value
 // return any left over data
+/*
 function Filter(input, props) {
   let filter = input.toUpperCase();
 
@@ -69,24 +133,23 @@ function Filter(input, props) {
       return;
   }
 }
+*/
 
-// display componet displays all data left over from SearchData
-function SearchResults() {
-  let data = [];
-  for (let i = 0; i < bookData.length; i++) {
-    data = [...data, bookData[i].Title];
-  }
-  const vars = bookData.map((book, index) => {
-    const { Title, Author } = book;
-    return <li>{(Title, Author)}</li>;
-  });
-  return (
-    <div>
-      {data}
-      <hr />
-      {vars}
+// gets state from parent
+//uses state value to call search function
+//gets returned array
+//loops through array desplaying the results
+function SearchResults(props) {
+  const { valueDrop = "keyword" } = props;
+  const { valueSearch = "" } = props;
+  let data = searchByKeyword(bookData, valueDrop, valueSearch);
+  console.log("DATA=", data);
+
+  return data.map((book, index) => (
+    <div key={index}>
+      {book.title}, {book.author}
     </div>
-  );
+  ));
 }
 
 //when typing in search bar, find matching results without showing user
